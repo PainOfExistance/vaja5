7100bd13a11302f641c96058c8d5aeddb5b27b49
PainOfExistance <matej113a@gmail.com>
2023-05-16 17:59:36 +0200

    neke

--- vaja4/vaja4.py
+++ vaja4/vaja4.py
@@ -1,209 +1,174 @@
-﻿import cv2
+﻿from email import utils
+import cv2
 import numpy as np
 import glob
-from numba import njit
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, classification_report
 import pandas as pd
-
-def razdeli_podatke(images, lowPercent):
-    learn = []
-    test = []
-    dist =  int(len(images) / 2)
-    catPercent = int(dist * (lowPercent/100))
-    dogPercent = int(dist * (lowPercent/100))
-    
-    for element in images[0:catPercent:1]:
-        learn.append(element)
-
-    for element in images[catPercent:dist:1]:
-        test.append(element)
-
-
-    for element in images[dist:dist+dogPercent:1]:
-        learn.append(element)
-
-    for element in images[dist+dogPercent:len(images):1]:
-        test.append(element)
-
-    return learn, test
+from joblib import dump, load
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+import os
+import tensorflow as tf
 
 def lbp(image):
+
     height, width = image.shape
-    lbp_image = np.zeros((height - 2, width - 2), dtype=np.uint8)
-    
-    center = image[1:-1, 1:-1]
-    
-    # Create a 3D array to store the 8 neighbors for each pixel
-    neighbors = np.stack([image[:-2, :-2], image[:-2, 1:-1], image[:-2, 2:],
-                          image[1:-1, :-2],                 image[1:-1, 2:],
-                          image[2:, :-2],   image[2:, 1:-1], image[2:, 2:]], axis=-1)
-    
-    # Calculate binary pattern
-    binary_pattern = (neighbors >= center[..., np.newaxis]).astype(int)
-    
-    # Calculate LBP values using vectorized operations
-    powers_of_two = np.array([1, 2, 4, 8, 16, 32, 64, 128], dtype=np.uint8)
-    lbp_image = np.sum(binary_pattern * powers_of_two, axis=-1)
-    
+    lbp_image = np.zeros((height, width), dtype=np.uint8)
+
+    for ih in range(1, height - 1):
+        for iw in range(1, width - 1):
+            center = image[ih, iw]
+            neighbors = [
+                image[ih - 1, iw - 1],
+                image[ih - 1, iw],
+                image[ih - 1, iw + 1],
+                image[ih, iw - 1],
+                image[ih, iw + 1],
+                image[ih + 1, iw - 1],
+                image[ih + 1, iw],
+                image[ih + 1, iw + 1],
+            ]
+
+            binary_pattern = (np.array(neighbors) >= center).astype(int)
+            powers_of_two = np.array([1, 2, 4, 8, 16, 32, 64, 128], dtype=np.uint8)
+            lbp_value = np.sum(binary_pattern * powers_of_two)
+
+            lbp_image[ih, iw] = lbp_value
+
     return lbp_image
 
-def hog(gray_image, vel_celice, vel_blok, razdelki):
-    gx = cv2.Sobel(gray_image, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5)
-    gy = cv2.Sobel(gray_image, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5)
-    M = np.sqrt(gx**2 + gy**2)
-    S = np.arctan2(gy, gx) * (180 / np.pi) % 180
 
-    bins = []
-    y_range = range(0, S.shape[0] - vel_celice * vel_blok + 1, vel_celice)
-    x_range = range(0, S.shape[1] - vel_celice * vel_blok + 1, vel_celice)
+def hog(gray_image, cell_size, block_size, num_bins):
+    height, width = gray_image.shape
+    gx = np.zeros_like(gray_image, dtype=np.float32)
+    gy = np.zeros_like(gray_image, dtype=np.float32)
+
+    gx[:, :-1] = np.diff(gray_image, n=1, axis=1)
+    gy[:-1, :] = np.diff(gray_image, n=1, axis=0)
+
+    gradient_magnitude = np.sqrt(gx ** 2 + gy ** 2)
+    gradient_orientation = (np.arctan2(gy, gx) * (180.0 / np.pi) + 180.0) % 180.0
+
+    num_cells_x = width // cell_size
+    num_cells_y = height // cell_size
+
+    histogram = np.zeros((num_cells_y, num_cells_x, num_bins))
+
+    for y in range(num_cells_y):
+        for x in range(num_cells_x):
+            cell_magnitude = gradient_magnitude[y * cell_size:(y + 1) * cell_size,
+                             x * cell_size:(x + 1) * cell_size]
+            cell_orientation = gradient_orientation[y * cell_size:(y + 1) * cell_size,
+                               x * cell_size:(x + 1) * cell_size]
+            
+            hist = np.histogram(cell_orientation, bins=num_bins, range=(0, 180),
+                                weights=cell_magnitude)[0]
+
+            histogram[y, x, :] = hist / np.sqrt(np.sum(hist ** 2) + 1e-6)
+
+    hog_descriptor = np.zeros((num_cells_y - block_size + 1, num_cells_x - block_size + 1,
+                               block_size * block_size * num_bins))
+
+    for y in range(num_cells_y - block_size + 1):
+        for x in range(num_cells_x - block_size + 1):
+            block_histogram = histogram[y:y + block_size, x:x + block_size, :].flatten()
+            hog_descriptor[y, x, :] = block_histogram / np.sqrt(np.sum(block_histogram ** 2) + 1e-6)
+
+    return hog_descriptor.flatten()
+
+
+
+img = []
+fname = []
+for file in glob.glob(
+    "G:/Faks/Sola/2.letnik/2.semester/rv/vaja4/images/*.jpg"
+):
+    tmp=os.path.splitext(os.path.basename(file))[0]
+    fname.append(tmp[:3])
+    img.append(cv2.imread(file))
     
-    for y in y_range:
-        for x in x_range:
-            block_bins = np.zeros(razdelki)
-            subarea_M = M[y:y + vel_celice * vel_blok, x:x + vel_celice * vel_blok]
-            subarea_S = S[y:y + vel_celice * vel_blok, x:x + vel_celice * vel_blok]
-            z_range = range(0, vel_celice * vel_blok, vel_celice)
-            
-            for z in z_range:
-                for u in z_range:
-                    cell_M = subarea_M[z:z + vel_celice, u:u + vel_celice]
-                    cell_S = subarea_S[z:z + vel_celice, u:u + vel_celice]
-                    hist, _ = np.histogram(cell_S, bins=razdelki, range=(0, 180), weights=cell_M)
-                    block_bins += hist
-            
-            # Normalize the block histogram using L2-norm
-            l2_norm = np.sqrt(np.sum(block_bins ** 2))
-            normalized_block_bins = block_bins / l2_norm
-            bins.append(normalized_block_bins)
 
-    # Flatten the list of block histograms
-    flattened_histograms = np.hstack(bins)
-    return flattened_histograms
-
-img = [cv2.imread(file) for file in glob.glob("G:/Faks/Sola/2.letnik/2.semester/rv/vaja4/images/*.jpg")]
 vel_celice = int(input("vnestie vel_celice:"))
 vel_blok = int(input("vnestie vel_blok:"))
 razdelki = int(input("vnestie število razdelkov:"))
 lowPercent = int(input("vnesite % učenja:"))
 
-images=[]
-for z in img:
-    if(z is not None):
-        z=cv2.resize(z,(100,100))
+images = []
+animals = []
+for z, n in zip(img,fname):
+    if z is not None:
+        z = cv2.resize(z, (100, 100))
         images.append(z)
+        if n=="cat":
+            animals.append(0)
+        else:
+            animals.append(1)
     else:
         print(z)
 
-        """
-learn, test=razdeli_podatke(images, lowPercent)
-catsHOG=[]
-catsLBP=[]
-dogsHOG=[]
-dogsLBP=[]
-
-catsHOGtest=[]
-catsLBPtest=[]
-dogsHOGtest=[]
-dogsLBPtest=[]
-
-for x in learn[0:int(len(learn)/2):1]:
-    gray_image= cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
-    catsHOG.append(hog(gray_image, vel_celice, vel_blok, razdelki))
-    catsLBP.append(lbp(gray_image))
-
-for x in learn[int(len(test)/2)+1:int(len(learn)):1]:
-    gray_image= cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
-    dogsHOG.append(hog(gray_image, vel_celice, vel_blok, razdelki))
-    dogsLBP.append(lbp(gray_image))
-
-
-for x in test[0:int(len(test)/2):1]:
-    gray_image= cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
-    catsHOGtest.append(hog(gray_image, vel_celice, vel_blok, razdelki))
-    catsLBPtest.append(lbp(gray_image))
-
-for x in test[int(len(test)/2)+1:int(len(test)):1]:
-    gray_image= cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
-    dogsHOGtest.append(hog(gray_image, vel_celice, vel_blok, razdelki))
-    dogsLBPtest.append(lbp(gray_image))
-
-
-for x in dogsHOG:
-    catsHOG.append(x)
-
-for x in dogsLBP:
-    catsLBP.append(x)
-
-for x in dogsHOGtest:
-    catsHOG.append(x)
-
-for x in dogsLBPtest:
-    catsLBP.append(x)
-    """
-
-catsHOG=[]
-catsLBP=[]
-for x in images:
-    gray_image= cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
-    catsHOG.append(hog(gray_image, vel_celice, vel_blok, razdelki))
-    catsLBP.append(lbp(gray_image))
-
-
-# Assuming catsHOG and catsLBP are lists of HOG and LBP features for each image
-combined_learn = []
-for hog_feat, lbp_feat in zip(catsHOG, catsLBP):
-    # Flatten the HOG and LBP features
-    hog_feat_flat = hog_feat.flatten()
-    lbp_feat_flat = lbp_feat.flatten()
-
-    # Concatenate the flattened features
-    combined_feat = np.hstack((hog_feat_flat, lbp_feat_flat))
-    combined_learn.append(combined_feat)
-
-# Create labels
-animals=[]
-for i in range(int(len(images))):
-    if(i<500):
-        animals.append("cat")
-    else:
-        animals.append("dog")
 labels = np.array(animals)
 
-# Pad the shorter array with zeros (if necessary)
-max_length = max(len(combined_learn), len(labels))
-if len(combined_learn) < max_length:
-    combined_learn += [np.zeros_like(combined_learn[0])] * (max_length - len(combined_learn))
-elif len(labels) < max_length:
-    labels = np.hstack((labels, [None] * (max_length - len(labels))))
+vect = []
+i=0
+for img in images:
+    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    lbp_image = lbp(gray_image)
+    hog_descriptor = hog(gray_image, vel_celice, vel_blok, razdelki)
+    feature_vector = np.concatenate((lbp_image.flatten(), hog_descriptor))
+    vect.append(feature_vector)
+    print(i)
+    i=i+1
 
-# Replace missing values (NaN) with the column mean
-combined_learn_df = pd.DataFrame(combined_learn)
-combined_learn_df.fillna(combined_learn_df.mean(), inplace=True)
-combined_learn_clean = combined_learn_df.values.tolist()
+# Convert data to numpy arrays
+vect = np.array(vect)
 
-# Split the data and train the classifier
-X_train, X_test, y_train, y_test = train_test_split(combined_learn_clean, labels, test_size=(lowPercent/100), random_state=2)
-knn = KNeighborsClassifier(n_neighbors=3)
-knn.fit(X_train, y_train)
 
-# Make predictions on the testing set
-y_pred = knn.predict(X_test)
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(vect, labels, test_size=((100-lowPercent)/100), random_state=80, stratify=labels)
 
-# Evaluate the classifier
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy:.2f}")
-print("\nClassification report:")
-print(classification_report(y_test, y_pred))
+# One-hot encode labels
+num_classes = len(np.unique(labels))
+y_train_encoded = tf.keras.utils.to_categorical(y_train, num_classes)
+y_test_encoded =  tf.keras.utils.to_categorical(y_test, num_classes)
 
-cv2.imshow("Edge Detection", images[0]);
-cv2.waitKey(0);
-cv2.destroyAllWindows();
+# Define neural network architecture
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(128, activation='relu', input_shape=(X_train.shape[1],)),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(num_classes, activation='softmax')  # Update the number of output units
+])
 
+# Compile model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train model
+model.fit(X_train, y_train_encoded, epochs=10, batch_size=32, validation_data=(X_test, y_test_encoded))
+
+# Evaluate model
+test_loss, test_acc = model.evaluate(X_test, y_test_encoded)
+print('Test accuracy:', test_acc)
+
+# Use model for prediction
+predictions = model.predict(vect)
 
 
 """
+catsHOG = []
+catsLBP = []
+i=0
+for x in images:
+    gray_image = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
+    catsHOG.append(hog(gray_image, vel_celice, vel_blok, razdelki))
+    catsLBP.append(lbp(gray_image))
+    print(i)
+    i=i+1
+labels = np.array(animals)
+
+
 # Assuming catsHOG and catsLBP are lists of HOG and LBP features for each image
 combined_learn = []
 for hog_feat, lbp_feat in zip(catsHOG, catsLBP):
@@ -215,14 +180,13 @@ for hog_feat, lbp_feat in zip(catsHOG, catsLBP):
     combined_feat = np.hstack((hog_feat_flat, lbp_feat_flat))
     combined_learn.append(combined_feat)
 
-# Create labels
-animals = ["cat"] * (len(images) // 2) + ["dog"] * (len(images) // 2)
-labels = np.array(animals)
 
 # Pad the shorter array with zeros (if necessary)
 max_length = max(len(combined_learn), len(labels))
 if len(combined_learn) < max_length:
-    combined_learn += [np.zeros_like(combined_learn[0])] * (max_length - len(combined_learn))
+    combined_learn += [np.zeros_like(combined_learn[0])] * (
+        max_length - len(combined_learn)
+    )
 elif len(labels) < max_length:
     labels = np.hstack((labels, [None] * (max_length - len(labels))))
 
@@ -232,8 +196,13 @@ combined_learn_df.fillna(combined_learn_df.mean(), inplace=True)
 combined_learn_clean = combined_learn_df.values.tolist()
 
 # Split the data and train the classifier
-X_train, X_test, y_train, y_test = train_test_split(combined_learn_clean, labels, test_size=(lowPercent/100), random_state=42)
-knn = KNeighborsClassifier(n_neighbors=3)
+X_train, X_test, y_train, y_test = train_test_split(
+    combined_learn_clean, labels, test_size=((100-lowPercent) / 100), random_state=90, stratify=labels
+)
+# knn=SVC(kernel='poly', C=5, probability=True)
+# knn = KNeighborsClassifier(n_neighbors=8)
+knn = RandomForestClassifier(n_estimators=1000, random_state=80)
+
 knn.fit(X_train, y_train)
 
 # Make predictions on the testing set
@@ -244,4 +213,5 @@ accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy:.2f}")
 print("\nClassification report:")
 print(classification_report(y_test, y_pred))
+dump(knn, "model7.joblib")
 """
